96bf9cee6e1801af6784d83f86d435a7387975f7
Yue Niu <yue@yues-air.wv.cc.cmu.edu>
2015-01-15 00:36:24 -0500

    p21

--- p20.c
+++ p20.c
@@ -4,8 +4,35 @@
 #include <math.h>
 #include <gmp.h>
 
+mpz_t factorial(mpz_t num)
+{
+  
+  while(count <= num)
+  {
+    prod = prod * count;
+    count++;
+  }
+
+  return prod;
+}
+
+unsigned int sum(unsigned long long num)
+{
+  unsigned int sum = 0;
+  while(num > 0)
+  {
+    sum += num % 10;
+    num = num / 10;
+  }
+
+  return sum;
+}
 int main()
 {
+  for (int i = 0; i < 20; ++i)
+  {
+    printf("%d %llu %u\n", i, factorial(i), sum(factorial(i)));
+  }
   
   return 0;
 }
--- p21.c
+++ p21.c
@@ -3,8 +3,36 @@
 #include <stdbool.h>
 #include <math.h>
 
+unsigned int div_sum(unsigned int num)
+{
+  unsigned int sum = 0;
+
+  for (int i = 1; i <= num / 2; i++)
+  {
+    if (num % i == 0)
+    {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
 int main()
 {
-  
+  int count = 0;
+  for (int i = 1; i < 10000; i++)
+  {
+    for (int j = i; j < 10000; j++)
+    {
+      if (i == div_sum(j) && div_sum(i) == j && i != j)
+      {
+        printf("%d  %d\n", i, j); 
+        count++;     
+      }
+    }
+    
+
+  }
+  printf("%d\n", count);
   return 0;
 }
